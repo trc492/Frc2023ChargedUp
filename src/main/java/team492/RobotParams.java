c770f3a41c632fd0fae182f35b0a7cb4404a2566
Titan Robotics Club <trc492@users.noreply.github.com>
2023-01-19 02:43:36 -0800

    Updated CommonLib. Turned off streaming camera but turned on both PhotonVision and OpenCvVision.

--- src/main/java/team492/RobotParams.java
+++ src/main/java/team492/RobotParams.java
@@ -43,10 +43,10 @@ public class RobotParams
         public static final boolean useNavX                     = false;
         public static final boolean usePdp                      = false;
         // Vision
-        public static final boolean usePhotonVision             = true;
-        public static final boolean useLimeLightVision          = false;
-        public static final boolean useOpenCvVision             = true;
         public static final boolean useStreamCamera             = false;
+        public static final boolean usePhotonVision             = true;
+        public static final boolean useOpenCvVision             = true;
+        public static final boolean useLimeLightVision          = false;
         // Robot
         public static final boolean noRobot                     = true;
         // Drive Base
